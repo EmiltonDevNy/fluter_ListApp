9a2bc1e16e026878ac38707b00ebc80fc120298c
EmiltonDevNy <emanuel.silva@nytec.com.br>
2019-12-05 15:07:54 -0400

    Adicionado - Função adicionar na lista

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,20 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _toDoList = ["Danie", "Emanuel"];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +47,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: _toDoController,
                     decoration: InputDecoration(
                         labelText: "Nova Tarefa",
                         labelStyle: TextStyle(color: Colors.blueAccent)
@@ -44,7 +58,7 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: (){},
+                  onPressed: _addToDo,
                 )
               ],
             ),
@@ -54,9 +68,18 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
                 itemBuilder: (context, index){
-                  return ListTile(
-                    title: Text(_toDoList[index]),
-                  );
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon( _toDoList[index]["ok"] ?
+                          Icons.check : Icons.error),),
+                    onChanged: (c) {
+                      setState(() {
+                        _toDoList[index]["ok"] = c;
+                      });
+                    },
+                    );
                 }),
           )
         ],

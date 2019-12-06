944ba8ef2ffb0ad460472804c56f1cef032be997
EmiltonDevNy <emanuel.silva@nytec.com.br>
2019-12-06 09:40:20 -0400

    FINAL - app finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,20 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,10 +93,11 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
+            child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
-                itemBuilder: buildItem),
+                itemBuilder: buildItem),),
           )
         ],
       ),
@@ -130,7 +145,7 @@ class _HomeState extends State<Home> {
           },),
           duration: Duration(seconds: 2),
         );
-
+        Scaffold.of(context).removeCurrentSnackBar();
         Scaffold.of(context).showSnackBar(snack);
       },
     );
